62aa5a17328d8b697e9b5e9625a97ee06f57d0a0
pr13260 <67130996+pr13260@users.noreply.github.com>
2020-12-02 01:18:29 +0530

    +3

--- config.py
+++ config.py
@@ -20,6 +20,6 @@ class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
  SOURCE="https://telegra.ph/file/c97d38a45cda9d5218943.jpg"
- START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n Made by @Cabots \ndevs @thestarkarmy13260" "
+ START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do  Made by @Cabots devs @thestarkarmy13260 "
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
